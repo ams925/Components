81d8e54fd3bb7b1af2756d756a3abe3613daec93
Asif Mahmud <58434895+ams925@users.noreply.github.com>
2023-07-09 17:02:22 +0600

    Create ProductInfo.jsx

---
+++ ProductInfo.jsx
@@ -0,0 +1,134 @@
+<div className='h-auto w-full p-10 bg-white rounded-md '>
+            <h1 class="text-2xl font-bold">Product Picture</h1>
+            <div class="grid grid-cols-1 gap-24 md:grid-cols-2 lg:grid-cols-4 my-10">
+                <div class="h-72 w-60">
+                    <img class="h-full w-full object-cover " src="https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="" />
+                </div>
+                <div class="h-72 w-60">
+                    <img class="h-full w-full object-cover " src="https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="" />
+                </div>
+                <div class="h-72 w-60">
+                    <img class="h-full w-full object-cover " src="https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="" />
+                </div>
+                <div class="h-72 w-60">
+                    <img class="h-full w-full object-cover " src="https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="" />
+                </div>
+                
+            </div>
+
+            <div className="flex flex-col w-full h-auto text-slate-800">
+                <div className="flex flex-col w-full h-auto my-2 py-5 px-5 justify-start  bg-slate-100 rounded  text-gray-500">
+                    <p className="text-xl px-2 font-medium py-2">Product Name: Charmeuse Satin Fabric | Silky Soft Satin | 60" Wide | Continuous Yards</p>
+                    <p className="text-xl px-2 font-medium py-2">Product Code: CHE-5</p>
+                </div>
+
+                <h1 class="text-2xl py-3 font-bold">Product Details</h1>
+                <div className="overflow-auto">
+                    <table className="w-full text-left table-auto font-normal " id="table-items">
+                        <thead>
+                            <tr className="bg-slate-100  text-gray-500  font-normal">
+                                <th className="px-2 py-1 text-right">Category</th>
+                                <th className="px-2 py-1 text-right">Sub Category</th>
+                                <th className="px-2 py-1 text-right">Type</th>
+                                <th className="px-2 py-1 text-right">Color</th>
+                                <th className="px-2 py-1 text-right">Bussiness to Customer</th>
+                                <th className="px-2 py-1 text-right">Bussiness to Bussiness</th>
+                                <th className="px-2 py-1 text-right">Tax</th>
+                                <th className="px-2 py-1 text-right">Discount</th>
+                                <th className="px-2 py-1 text-right">Shipping Cost</th>
+                                <th className="px-2 py-1 text-right">Unit</th>
+                                <th className="px-2 py-1 text-right">Stock Left</th>
+                                <th className="px-2 py-1 text-right">Total Sales</th>
+                                <th className="px-2 py-1 text-right">Threshold</th>
+                                <th className="px-2 py-1 text-right">Product Status</th>
+                                <th className="px-2 py-1 text-right">Last Updated</th>
+                            </tr>
+                        </thead><tr className="odd:bg-slate-100 even:bg-slate-200" />
+                        <tbody>
+                            <tr className="odd:bg-white even:bg-slate-200">
+                                <td className="px-4 py-1 text-right border">Silk</td>
+                                <td className="px-4 py-1 text-right border">Pure Silk</td>
+                                <td className="px-4 py-1 text-right border">Velvet</td>
+                                <td className="px-4 py-1 text-right border">Rose Milk</td>
+                                <td className="px-4 py-1 text-right border">14</td>
+                                <td className="px-4 py-1 text-right border">10</td>
+                                <td className="px-4 py-1 text-right border">5%</td>
+                                <td className="px-4 py-1 text-right border">5</td>
+                                <td className="px-4 py-1 text-right border">10</td>
+                                <td className="px-4 py-1 text-right border">1000</td>
+                                <td className="px-4 py-1 text-right border">521</td>
+                                <td className="px-4 py-1 text-right border">479</td>
+                                <td className="px-4 py-1 text-right border">100</td>
+                                <td className="px-4 py-1 text-right border">Active</td>
+                                <td className="px-4 py-1 text-right border">Sat Jul 08 2023</td>
+                            </tr>
+                            <tr className="odd:bg-white even:bg-slate-200">
+                                <td className="px-4 py-1 text-right border">Silk</td>
+                                <td className="px-4 py-1 text-right border">Pure Silk</td>
+                                <td className="px-4 py-1 text-right border">Velvet</td>
+                                <td className="px-4 py-1 text-right border">Rose Milk</td>
+                                <td className="px-4 py-1 text-right border">14</td>
+                                <td className="px-4 py-1 text-right border">10</td>
+                                <td className="px-4 py-1 text-right border">5%</td>
+                                <td className="px-4 py-1 text-right border">5</td>
+                                <td className="px-4 py-1 text-right border">10</td>
+                                <td className="px-4 py-1 text-right border">1000</td>
+                                <td className="px-4 py-1 text-right border">521</td>
+                                <td className="px-4 py-1 text-right border">479</td>
+                                <td className="px-4 py-1 text-right border">100</td>
+                                <td className="px-4 py-1 text-right border">Active</td>
+                                <td className="px-4 py-1 text-right border">Sat Jul 08 2023</td>
+                            </tr>
+                            <tr className="odd:bg-white even:bg-slate-200">
+                                <td className="px-4 py-1 text-right border">Silk</td>
+                                <td className="px-4 py-1 text-right border">Pure Silk</td>
+                                <td className="px-4 py-1 text-right border">Velvet</td>
+                                <td className="px-4 py-1 text-right border">Rose Milk</td>
+                                <td className="px-4 py-1 text-right border">14</td>
+                                <td className="px-4 py-1 text-right border">10</td>
+                                <td className="px-4 py-1 text-right border">5%</td>
+                                <td className="px-4 py-1 text-right border">5</td>
+                                <td className="px-4 py-1 text-right border">10</td>
+                                <td className="px-4 py-1 text-right border">1000</td>
+                                <td className="px-4 py-1 text-right border">521</td>
+                                <td className="px-4 py-1 text-right border">479</td>
+                                <td className="px-4 py-1 text-right border">100</td>
+                                <td className="px-4 py-1 text-right border">Active</td>
+                                <td className="px-4 py-1 text-right border">Sat Jul 08 2023</td>
+                            </tr>
+                            <tr className="odd:bg-white even:bg-slate-200">
+                                <td className="px-4 py-1 text-right border">Silk</td>
+                                <td className="px-4 py-1 text-right border">Pure Silk</td>
+                                <td className="px-4 py-1 text-right border">Velvet</td>
+                                <td className="px-4 py-1 text-right border">Rose Milk</td>
+                                <td className="px-4 py-1 text-right border">14</td>
+                                <td className="px-4 py-1 text-right border">10</td>
+                                <td className="px-4 py-1 text-right border">5%</td>
+                                <td className="px-4 py-1 text-right border">5</td>
+                                <td className="px-4 py-1 text-right border">10</td>
+                                <td className="px-4 py-1 text-right border">1000</td>
+                                <td className="px-4 py-1 text-right border">521</td>
+                                <td className="px-4 py-1 text-right border">479</td>
+                                <td className="px-4 py-1 text-right border">100</td>
+                                <td className="px-4 py-1 text-right border">Active</td>
+                                <td className="px-4 py-1 text-right border">Sat Jul 08 2023</td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+
+                <select className="select select-bordered w-full max-w-xs my-5">
+                    <option disabled selected>Customer's Choice</option>
+                    <option>A</option>
+                    <option>B</option>
+                    <option>S</option>
+                    <option>D</option>
+                    <option>F</option>
+                </select>
+
+                <div className='flex flex-col w-full h-auto my-5 py-5 px-5 justify-start  bg-slate-100 rounded  text-gray-500 '>
+                    <div class="w-full "><h1 class="text-2xl py-3 font-bold">Product Care Instruction</h1><div class="dark:text-white text-base text-gray-500 detailsProduct ql-editor" style={{ minHeight: "10px" }}><ul><li>Charmeuse Satin is a lightweight smooth solid satin.</li><li><a href="&quot;https://mytextilefabric.com/products/ifr-charmeuse-satin-yards&quot;" rel="&quot;noopener" target="&quot;_blank&quot;"><strong>Fabric by the Yard</strong></a>&nbsp;is sold with a min of 2 yards - a quantity of 10 means 10 yards.</li><li><a href="&quot;https://mytextilefabric.com/products/ifr-charmeuse-satin-bolts&quot;" rel="&quot;noopener" target="&quot;_blank&quot;"><strong>Fabric by the Bolt</strong></a>&nbsp;is sold 75 yards - a quantity of 1 means 1 bolt or 75 yards.</li><li>You may also request a&nbsp;<a href="&quot;https://mytextilefabric.com/products/ifr-charmeuse-satin-swatches&quot;" rel="&quot;noopener" target="&quot;_blank&quot;"><strong>3"x3" Sample Swatch</strong></a>.</li></ul><p><br /></p></div><h1 class="text-2xl py-3 font-bold">Product Information</h1><div class="dark:text-white text-base text-gray-500 detailsProduct ql-editor" style={{ minHeight: "10px" }}><p><strong>Inherently Fire Retardant (IFR):</strong>This fabric is Certified fire retardant NFPA 701.&nbsp;Tested and reviewed by Laboratories for Indoor Flame Retardancy.&nbsp;Fire Marshalls and or special jobs require certified fire retardant documents for approval before installation and or product development.&nbsp;This item passes the NFPA 701 testing. Some venues at events may also require this certification for event and or decorations.&nbsp;NFPA 701 Certificate available upon request.</p><p>Charmeuse Satin is a lightweight smooth solid satin. Our Charmeuse satin is very soft and luxurious like a silky satin but is 100% polyester. Our Charmeuse satin is so flowy that it makes it a great pipe n drape/swagging fabric for the event industry. Great for dresses, lining for wedding gowns and used a lot for lingerie's, robes, and blankets. This fabric is very versatile and has an economic price point.</p><p><strong>Cleaning Instructions</strong></p><p>100% Polyester Washable - Machine Cycle Cold or Warm, Low Tumble, Dry under low temperatures.&nbsp;</p><p><strong>Picture Taken</strong></p><p>Picture is taken with and/or without flash depending on the color. If you are very particular about the color tone - please order a sample swatch to avoid any issues.</p><p><strong>Fabric Weight</strong></p><p>Approximately 75 grams per Square Meter. Fabric has a very soft and silky hand texture with a light weight body.</p><p><strong>Solid or Sheer</strong></p><p>Solid</p><p><strong>Fabric Content</strong></p><p>100% Polyester - Very Soft Touch</p><p><strong>Fabric Width</strong></p><p>58/60"</p><p><strong>Fabric Origin</strong></p><p>Imported Product</p><p><br /></p><p><strong>Ships from Los Angeles, California within one/two Business Days | Sample Swatches may Ship One Day Later | Let us Know if you are in a Super Rush - We can&nbsp;Accommodate |</strong></p><p><strong>Lead Time</strong></p><p>Fabric Yard and Sample Swatch orders tend to ship out within a few days. Fabric Bolt and Color Cards ship out same or next day depending on availability.</p><p><strong>Color Waves</strong></p><p>We have a Strict no Return Policy on our orders - If you are very particular with a color - please order a sample swatch to make sure the color is correct.</p><p><strong>Dye Lot Variations</strong></p><p>Dye lots may vary from order to order - As fabric rolls finish - we open new rolls and dye lots may vary from time to time - please order a sample swatch to assure the current dye lot you will be receiving</p><p><strong>Full Piece and/or Splits</strong></p><p>We strive to give one full piece cut orders. At times our rolls end and or full pcs are not available and splits may occur. Some fabric rolls come straight from our mill unpackaged and untouched but will have a few splits in the rolls. We do not guarantee one full pieces - if this is an issue - please contact our reps prior to purchasing</p></div></div>
+                </div>
+            </div>
+        </div>
